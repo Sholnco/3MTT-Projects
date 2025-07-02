5262586fb4ded9f10e8e749287195bfe22e3ac85
Matthew Olushola Oke <107432064+Sholnco@users.noreply.github.com>
2025-07-02 07:46:29 +0100

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,9 +1,34 @@
+import './App.css';
+import { useState } from 'react';
+import './App.css';
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
+  };
+
+  const decrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>Click Counter</h1>
+      <h2>{count}</h2>
+
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+
+      {count === 10 && <p className="limit-message">You've reached the limit!</p>}
     </main>
   );
 }
 
 export default App;
+
